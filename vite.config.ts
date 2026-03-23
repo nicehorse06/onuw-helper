580bf023ddcd5c7ac89dc7423b85a396705d9d00
Jimmy Ma <jimmatw@gmail.com>
2026-03-23 11:40:55 +0800

    init github page settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const base = isGithubActions && repositoryName ? `/${repositoryName}/` : '/'
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base
 })
